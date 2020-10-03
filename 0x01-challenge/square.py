7c01beb1c3fe27e5ab80bacc1966a93ecd006ac0
Brett Davis <brett.p.davis101@gmail.com>
2020-10-03 20:11:15 +0000

    pep8 fixes, adds documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+'''Module Docstring'''
+
 
 class square():
-    
+    '''Class Docstring'''
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        '''Initializes width, height'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +17,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        '''Finds the area of the /rectangle/'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        '''String representation of the rectangle'''
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
